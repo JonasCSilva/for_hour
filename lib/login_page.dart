681d7c2acbb97a121fd76d51c620eef9b32759ce
matheus.kleinschmidt@catolicasc.edu.br <jonas02.silva@catolicasc.edu.br>
2022-10-06 19:31:52 -0300

    Login page file created

---
+++ lib/login_page.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const Text('Bem-vindo', style: TextStyle(fontSize: 50)),
+              const SizedBox(height: 120),
+              ConstrainedBox(
+                  constraints: const BoxConstraints.tightFor(width: 250),
+                  child: const TextField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Email Address',
+                    ),
+                  )),
+              const SizedBox(height: 30),
+              ConstrainedBox(
+                  constraints: const BoxConstraints.tightFor(width: 250),
+                  child: const TextField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Password',
+                    ),
+                  )),
+              const SizedBox(height: 60),
+              ConstrainedBox(
+                  constraints: const BoxConstraints.tightFor(width: 280, height: 50),
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                        backgroundColor: const Color(0xff9b1536),
+                        textStyle: const TextStyle(fontSize: 20)),
+                    onPressed: () {},
+                    child: const Text('Login'),
+                  )),
+            ]),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:for_hour/login_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,58 +13,6 @@ class MyApp extends StatelessWidget {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
       home: LoginPage(),
-    );
-  }
-}
-
-class LoginPage extends StatefulWidget {
-  const LoginPage({super.key});
-
-  @override
-  State<LoginPage> createState() => _LoginPageState();
-}
-
-class _LoginPageState extends State<LoginPage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              const Text('Bem-vindo', style: TextStyle(fontSize: 50)),
-              const SizedBox(height: 120),
-              ConstrainedBox(
-                  constraints: const BoxConstraints.tightFor(width: 250),
-                  child: const TextField(
-                    obscureText: true,
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: 'Email Address',
-                    ),
-                  )),
-              const SizedBox(height: 30),
-              ConstrainedBox(
-                  constraints: const BoxConstraints.tightFor(width: 250),
-                  child: const TextField(
-                    obscureText: true,
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: 'Password',
-                    ),
-                  )),
-              const SizedBox(height: 60),
-              ConstrainedBox(
-                  constraints: const BoxConstraints.tightFor(width: 280, height: 50),
-                  child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                        backgroundColor: const Color(0xff9b1536),
-                        textStyle: const TextStyle(fontSize: 20)),
-                    onPressed: () {},
-                    child: const Text('Login'),
-                  )),
-            ]),
-      ),
     );
   }
 }
