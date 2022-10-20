3662b0b39cb2cf9940a0f60b5587e03659fde4ad
JonasCSilva <55038425+JonasCSilva@users.noreply.github.com>
2022-10-20 16:22:27 -0300

    firestore implemented in register_page

--- lib/register_page.dart
+++ lib/register_page.dart
@@ -1,4 +1,7 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_picker/flutter_picker.dart';
+import 'package:for_hour/home_page.dart';
 
 class RegisterPage extends StatefulWidget {
   const RegisterPage({super.key});
@@ -8,13 +11,58 @@ class RegisterPage extends StatefulWidget {
 }
 
 class _RegisterPageState extends State<RegisterPage> {
+  final _formKey = GlobalKey<FormState>();
+  final _pickerData = ["Course", "Internship"];
+
+  final _hoursController = TextEditingController();
+  final _categoryController = TextEditingController();
+
+  _addCertificate() async {
+    if (_formKey.currentState!.validate()) {
+      DocumentReference doc = await FirebaseFirestore.instance
+          .collection("users/tIeMjnsFX2Ur69clWHgJvCxxltc2/certificates")
+          .add({
+        "hours": _hoursController.text,
+        "category": _categoryController.text,
+      });
+
+      if (!mounted) return;
+
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+        content: Text('DocumentSnapshot added with ID: ${doc.id}'),
+      ));
+
+      Navigator.of(context)
+          .push(MaterialPageRoute(builder: (context) => const HomePage()));
+    }
+  }
+
+  _showPicker() async {
+    final result = await Picker(
+      hideHeader: true,
+      adapter: PickerDataAdapter(pickerdata: _pickerData),
+      selectedTextStyle: const TextStyle(color: Color(0xff9b1536)),
+    ).showDialog(context);
+    if (result != null) {
+      _categoryController.text = _pickerData[result[0]];
+    }
+  }
+
+  @override
+  void dispose() {
+    _hoursController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        minimum: const EdgeInsets.symmetric(vertical: 50),
+        body: SafeArea(
+      minimum: const EdgeInsets.only(top: 50, bottom: 50),
+      child: Form(
+        key: _formKey,
         child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: <Widget>[
               Row(mainAxisAlignment: MainAxisAlignment.center, children: [
                 IconButton(
@@ -27,26 +75,40 @@ class _RegisterPageState extends State<RegisterPage> {
                     )),
                 const Text('Cadastro de horas', style: TextStyle(fontSize: 30)),
               ]),
-              const SizedBox(height: 90),
               ConstrainedBox(
                   constraints: const BoxConstraints.tightFor(width: 250),
-                  child: const TextField(
-                    decoration: InputDecoration(
+                  child: TextFormField(
+                    controller: _hoursController,
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter some text';
+                      } else if (double.tryParse(value) == null) {
+                        return 'Please enter a number';
+                      }
+                      return null;
+                    },
+                    decoration: const InputDecoration(
                       border: OutlineInputBorder(),
                       labelText: 'Horas feitas',
                     ),
                   )),
-              const SizedBox(height: 30),
               ConstrainedBox(
                   constraints: const BoxConstraints.tightFor(width: 250),
-                  child: const TextField(
-                    obscureText: true,
-                    decoration: InputDecoration(
+                  child: TextFormField(
+                    readOnly: true,
+                    onTap: () => _showPicker(),
+                    controller: _categoryController,
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter a value';
+                      }
+                      return null;
+                    },
+                    decoration: const InputDecoration(
                       border: OutlineInputBorder(),
                       labelText: 'Categoria',
                     ),
                   )),
-              const SizedBox(height: 60),
               ConstrainedBox(
                   constraints:
                       const BoxConstraints.tightFor(width: 210, height: 45),
@@ -55,15 +117,12 @@ class _RegisterPageState extends State<RegisterPage> {
                         backgroundColor: const Color(0xff9b1536),
                         textStyle: const TextStyle(fontSize: 20)),
                     onPressed: () {
-                      /* Navigator.of(context).push(MaterialPageRoute(
-                          builder: (context) => const HomePage())); */
                       ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
                         content: Text('Not implemented!'),
                       ));
                     },
                     child: const Text('Anexar certificado'),
                   )),
-              const SizedBox(height: 60),
               ConstrainedBox(
                   constraints:
                       const BoxConstraints.tightFor(width: 250, height: 50),
@@ -71,17 +130,11 @@ class _RegisterPageState extends State<RegisterPage> {
                     style: ElevatedButton.styleFrom(
                         backgroundColor: const Color(0xff9b1536),
                         textStyle: const TextStyle(fontSize: 28)),
-                    onPressed: () {
-                      /* Navigator.of(context).push(MaterialPageRoute(
-                          builder: (context) => const HomePage())); */
-                      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-                        content: Text('Not implemented!'),
-                      ));
-                    },
+                    onPressed: () => _addCertificate(),
                     child: const Text('Enviar'),
                   )),
             ]),
       ),
-    );
+    ));
   }
 }
